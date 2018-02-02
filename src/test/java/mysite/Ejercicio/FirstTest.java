7ccc54d2e179d3602ff2800a7f6a58c42c15716a
Arnold Huanca <jahvtrek@gmail.com>
2018-02-02 18:19:19 -0400

    Adding WebDriverWait

--- src/test/java/mysite/Ejercicio/FirstTest.java
+++ src/test/java/mysite/Ejercicio/FirstTest.java
@@ -16,18 +16,31 @@ public class FirstTest {
         driver.get("http://www.collegeview.com/");
         driver.manage().window().maximize();
         driver.findElement(By.cssSelector("a[alt=\"SuperMatch\"] > img")).click();
+		
+        WebDriverWait w = new WebDriverWait(driver,15);
         driver.switchTo().frame("supermatch");
-        driver.findElement(By.id("critHeader0")).click();
-        //driver.findElements(By.id("locationCriteriaClearButton"));
-        driver.findElement(By.xpath("//span[contains(text(),'Ohio')]")).click();
 
+        driver.findElement(By.id("critHeader0")).click();
+        WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Ohio')]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        driver.findElement(By.xpath("//*[@id=\"locationCriteria\"]/div[1]/div/div[2]/div[47]")).click();
+        driver.findElements(By.id("locationCriteriaClearButton"));
+        driver.findElement(By.xpath("//*[@id=\"locationCriteria\"]/div[1]/div/div[2]/div[47]")).click();
+
+
+        driver.findElement(By.id("critHeader1")).click();
         driver.findElement(By.id("critHeader1")).click();
         driver.findElement(By.id("degreeTypeRadio0")).click();
         driver.findElement(By.id("majorSearchText")).sendKeys("Computer Software Engineering");
-        driver.findElement(By.xpath("//span[contains(text(),  'Computer Software Engineering')]")).click();
+        WebDriverWait wait2 = new WebDriverWait(driver, 60);
+        WebElement ele2 = wait2.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(text(),  'Computer Software Engineering')]")));
+        ele2.click();
 
-        String Univ  = driver.findElement(By.cssSelector("#result1100738 > div.column.column3 > div.nameLogoBlock > a")).getText();
+        String Univ = driver.findElement(By.cssSelector("#result1100738 > div.column.column3 > div.nameLogoBlock > a")).getText();
+        String Porc = driver.findElement(By.xpath("//*[@id=\"result1100738\"]/div[1]/div/div")).getText();
+
         Assert.assertTrue(Univ.equals("Miami University-Oxford"));
+        Assert.assertTrue(Porc.equals("100%"));
 
         //driver.close();
         // driver.quit();
